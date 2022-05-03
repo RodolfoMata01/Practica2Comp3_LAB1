ce25ee43547bc40058dc1cbc4d16a927b72e8402
RodolfoMata01 <matamurillo1234@gmail.com>
2022-05-03 14:49:07 -0600

    actulizacion

--- api2.js
+++ api2.js
@@ -1,26 +1,25 @@
 const fetch = require('node-fetch')
+const fs = require('fs');
 async function getBitcoin(){
     try{
         const response = await fetch ('https://api.coindesk.com/v1/bpi/currentprice.json');
-        const data = await response();
+        const data = await response.json();
         let bitcoinprice = "";
         Array.from(data).forEach(bit => {
             bitcoinprice+= `${bit['rate']}`;
         });
 
-        fs.writefile('bitcoint.txr', bitcoinprice, (error) => {
+        fs.writeFile('bitcoint.csv', bitcoinprice, (error) => {
             if (error){
                 console.log(error);
                 return;
             }
             console.log('se ha creado el archivo');
         })
-
         
     }catch(error){
         console.log(error)
     }
-    
 }
 getBitcoin();
 
